faba560defb610a3782240bb7929e8f51ff37954
yun-goon <sirius584@naver.com>
2022-09-16 15:05:48 +0900

    9.16

--- 2.py
+++ 2.py
@@ -1,6 +1,39 @@
-x = int(input("x :"))
-y = int(input("y :"))
-z = int(input("z :"))
+'''
+print( 200,'+',300,'+',400, '=', 200+300+400)
 
-avg = (x+y+z)/3
-print("avg = ", avg)
+width = 30
+height = 60
+
+width,height = 30, 60
+
+width = 40
+height = 20
+print("triangle : ", width*height/2)
+
+n1 = int(input("suqre's long : "))
+print("square : ", n1*n1)
+
+
+print("sum 1 to 10 : ",1+2+3+4+5+6+7+8+9+10)
+
+print("10! = ", 1*2*3*4*5*6*7*8*9*10)
+
+
+c = int(input("c : "))
+f = (9/5)*c +32
+
+print("C : ",c, " F : ", f)
+
+
+
+PI = 3.141592
+
+r = int(input("r : ?"))
+print(" round :" , 2*r*PI, " area ; ", r*r*PI)
+'''
+
+print("It's really hot!")
+print("""I said "Hello!"to him.""")
+print('''He said "What's there?"''')
+print("Newline character is \\n, Tab character is \\t")
+print("Working directory is C:\workspace\mywork.")
