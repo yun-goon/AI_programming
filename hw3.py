85c092e84efc5dfc0d9cc4cffaf96756a91d2e55
yun-goon <sirius584@naver.com>
2022-10-08 23:19:27 +0900

    조금 수정

--- hw3.py
+++ hw3.py
@@ -1,3 +1,12 @@
+'''
+참고할만한 사이트
+오류 해결
+https://hyunsooworld.tistory.com/entry/%EC%85%80%EB%A0%88%EB%8B%88%EC%9B%80-%EC%98%A4%EB%A5%98-AttributeError-WebDriver-object-has-no-attribute-findelementbycssselector-%EC%98%A4%EB%A5%98%ED%95%B4%EA%B2%B0
+예제
+https://sw-ko.tistory.com/37
+
+'''
+
 import wordcloud
 from selenium import webdriver
 import matplotlib.pyplot as plt
@@ -11,10 +20,11 @@ browser.get(url)
 
 time.sleep(2)
 
-#전체
+
+#페이지 전체
 # txt = browser.find_elements(By.XPATH,"/html/body/div/div/div[2]/div/div/div/div/div/div/div/div/div[1]/div[5]/div[5]/div/div/div/div/div/div/div/div[14]/div/div/div/div/div/div[1]/div")
 
-#일부
+#일부 - 1번 문단
 txt = browser.find_elements(By.XPATH,"//*[@id='jWZtJ8Cjb']/div[2]/div/div/div/div/div/div/div/div/div[1]/div[5]/div[5]/div/div/div/div/div/div/div/div[14]/div/div/div/div/div/div[1]/div/div[6]/div")
 
 for text in txt:
